a9c596cb826f48afc3e777fd59165c91f65bd2e7
sehandev <sehandev@gmail.com>
2022-12-28 17:19:53 +0900

    Calculate coherence with nsp

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Evaluation for Video Captioning
 
-Evaluate datasets for video captioning
+Evaluate coherence for video captioning
 
 
 ## TODO
-- [ ]  Load model (v0.3.0)
+- [x]  Calculate coherence between captions by calculating nsp score (v0.4.0)
+- [x]  Load model (v0.3.0)
 - [x]  Preprocess dataset (v0.2.0)
 - [x]  Init project (v0.1.0)
 
@@ -20,5 +21,5 @@ $ poetry install
 
 ```bash
 $ poetry run python evaluation.py \
-    --data_dir <path>/data/ \
+    --data_dir <path>/data/
 ```
--- evaluation.py
+++ evaluation.py
@@ -1,14 +1,16 @@
-import torch
+import json
 from tqdm.auto import tqdm
 from libs.model import ModelManager
 from libs.preprocess import load_data
 
 DATA_DIR = "./data"
 DATA_NAME = "descriptions_train_val.csv"
+OUTPUT_DIR = "./output"
 
 
-def calculate_similarity(sentence1: str, sentence2: str) -> float:
-    return 0.0
+def save_coherence_to_json(coherence_dict: dict):
+    with open(f"{OUTPUT_DIR}/coherence.json", "w") as coherence_file:
+        json.dump(coherence_dict, coherence_file, indent=2)
 
 
 def main():
@@ -21,13 +23,30 @@ def main():
             "caption_from_old": "caption",
         },
     )
-    manager = ModelManager(model_name="roberta-base")
-    for _, row in tqdm(data_df.iterrows(), total=len(data_df)):
-        cls_vector = manager.get_cls_vector(row["caption"])
-        torch.save(cls_vector, f"{DATA_DIR}/cls/{row.clip_id}.pt")
+    manager = ModelManager(model_name="bert-base-uncased")
 
-    # similarity = calculate_similarity("abc", "bcd")
-    # print(similarity)
+    # Calculate coherence between sentence
+    coherence_dict = dict()
+    for _, front_row in tqdm(
+        data_df.iterrows(),
+        desc="Coherence",
+        total=len(data_df),
+        position=0,
+    ):
+        coherence_dict[front_row.clip_id] = dict()
+        front_sentence = front_row.caption
+        for _, back_row in tqdm(
+            data_df.iterrows(),
+            total=len(data_df),
+            position=1,
+            leave=False,
+        ):
+            back_sentence = back_row.caption
+            coherence_score = manager.get_nsp_score(front_sentence, back_sentence)
+            coherence_score = coherence_score[0][0].item()
+            coherence_dict[front_row.clip_id][back_row.clip_id] = coherence_score
+
+    save_coherence_to_json(coherence_dict)
 
 
 if __name__ == "__main__":
