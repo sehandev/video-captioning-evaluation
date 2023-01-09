0e5040bb3d0746df58c71794db1689ce119f1720
sehandev <sehandev@gmail.com>
2023-01-09 16:55:12 +0900

    Edit data caching

--- data_caching.py
+++ data_caching.py
@@ -1,11 +1,25 @@
 from pathlib import Path
+from libs.preprocess import load_data
 
 DATA_DIR = Path("/data/coherence_evaluation")
 CACHE_DIR = DATA_DIR / "cache"
+DATA_NAME = "desc_and_plot_train_val.csv"
 
 
 def main():
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+    data_df = load_data(
+        data_dir=DATA_DIR,
+        data_filename=DATA_NAME,
+        rename_dict={
+            "videoid": "video_id",
+            "imdbid": "movie_id",
+            "caption_from_old": "caption",
+            "synopsis": "plot",
+        },
+    )
+    print(data_df)
 
 
 if __name__ == "__main__":
