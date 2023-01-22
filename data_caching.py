02d3de0c23d44ae47f85608a3162d8f5bc35a94c
sehandev <sehandev@gmail.com>
2023-01-22 18:33:13 +0900

    Add caching data system

--- data_caching.py
+++ data_caching.py
@@ -1,13 +1,23 @@
 from pathlib import Path
+import torch
+from tqdm.auto import tqdm
+from libs.model import ModelManager
 from libs.preprocess import load_data
+from libs.sentence_vector import generate_sentence_vectors
 
 DATA_DIR = Path("/data/coherence_evaluation")
 CACHE_DIR = DATA_DIR / "cache"
+CACHE_PLOT_DIR = CACHE_DIR / "plot"
+CACHE_CAPTION_DIR = CACHE_DIR / "caption"
 DATA_NAME = "desc_and_plot_train_val.csv"
 
 
-def main():
+def main(
+    model_name: str = "bert-base-uncased",
+):
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
+    CACHE_PLOT_DIR.mkdir(parents=True, exist_ok=True)
+    CACHE_CAPTION_DIR.mkdir(parents=True, exist_ok=True)
 
     data_df = load_data(
         data_dir=DATA_DIR,
@@ -19,8 +29,26 @@ def main():
             "synopsis": "plot",
         },
     )
-    print(data_df)
+
+    manager = ModelManager(model_name)
+
+    for _, row in tqdm(
+        data_df.iterrows(),
+        desc="Caching",
+        total=len(data_df),
+    ):
+        # Plot
+        vector_path = CACHE_PLOT_DIR / f"{row['movie_id']}.pt"
+        if not vector_path.exists():
+            plot_vector = generate_sentence_vectors(row["plot"], manager)
+            torch.save(plot_vector, vector_path)
+
+        # Caption
+        vector_path = CACHE_CAPTION_DIR / f"{row['video_id']}.pt"
+        if not vector_path.exists():
+            caption_vector = generate_sentence_vectors(row["caption"], manager)
+            torch.save(caption_vector, vector_path)
 
 
 if __name__ == "__main__":
-    main()
+    main(model_name="bert-base-uncased")
